e18f908769c0ec365364731c6198b68da35126ad
Jenny Lai <jenny.lai@flatironschool.com>
2017-01-30 16:09:17 -0500

    almost done

--- board.js
+++ board.js
@@ -13,16 +13,15 @@ class Board{
   startGame(){
     let music = document.createElement('audio')
     music.setAttribute('src', 'audio/Earth Wind and Fire- Shining Star - from YouTube.mp3')
+    this.rock = new Rock()
     document.addEventListener("click",
-    function() {
+    () => {
       music.play()
       $("#start").remove()
-      let rock = new Rock()
-      rock.render()
-      rock.moveRock()
+      this.rock.render()
+      this.rock.moveRock()
     })
   }
-
 
   checkCollision(){
   var collide = $('#dog').collision('.rock')
@@ -32,23 +31,41 @@ class Board{
   var collide4 = $('#dog').collision('.rock_4')
   var collide5 = $('#dog').collision('.rock_5')
     if (!!collide.length || !!collide1.length || !!collide2.length || !!collide3.length || !!collide4.length || !!collide5.length) { // true
-      alert('You lose!')
+      alert('YOU LOSE! GAME OVER')
+      alert = function(){}
       let loserMusic = document.createElement('audio')
       loserMusic.setAttribute('src', 'audio/Lost-life-sound-effect.mp3')
       loserMusic.play()
+      //this.endGame()
     }
   }
 
   collided(){
     let boardItself = this
-    setInterval(function(){
+    this.checkCollisionInterval = setInterval(function() {
       boardItself.checkCollision()}, 100)
   }
 
   winGame() {
-    if (($('#dog')[0].style['left']) == '950px') { // true
-      alert("YAY! YOU WIN!!!")
+    if ($('#dog')[0].style['left'] == '950px') { // true
+      alert('YAY! YOU WIN!!!')
+      alert = function(){}
+      let winMusic = document.createElement('audio')
+      winMusic.setAttribute('src', 'audio/Level-up-sound-effect.mp3')
+      winMusic.play()
+      //this.endGame()
     }
   }
+
+  win() {
+    var boardItself = this
+    //this.checkForWinInterval = setInterval(function(){boardItself.winGame()}, 100)
+    setInterval(function(){boardItself.winGame()}, 100)
+  }
+
+  // endGame() {
+  //   this.rock.stopRock()
+  // }
+
 
 }
--- rock.js
+++ rock.js
@@ -18,28 +18,37 @@ class Rock {
   }
 
   randomizedPosition() {
-    var h = $('#board').height() - 25 // 650-15 = H: 635
-    var newH = Math.floor(Math.random() * h) // .5 * 635 = 317
-    //var w = $('#board').width() - 100 // 1100 - 15 = W: 1085
-    //var newW = Math.floor(Math.random() * w) // .35 * 1085 = 379.75
-    //return [newH, newW]
+    var h = $('#board').height() - 15
+    var newH = Math.floor(Math.random() * h)
     return newH
+  }
+
+  anotherRandoPosition() {
+    var w = $('#board').width() - 50
+    var newW = Math.floor(Math.random() * w)
+    return newW
   }
 
   animateRock() {
     var newPosition = this.randomizedPosition()
-    $('.rock').animate({ bottom: newPosition, right: 100 }, {duration: 5000})
-    $('.rock_1').animate({ bottom: newPosition, right: 300 }, {duration: 5950})
-    $('.rock_2').animate({ bottom: newPosition, right: 400 }, {duration: 5800})
-    $('.rock_3').animate({ bottom: newPosition, right: 550 }, {duration: 5000})
-    $('.rock_4').animate({ bottom: newPosition, right: 750 }, {duration: 5050})
-    $('.rock_5').animate({ bottom: newPosition, right: 850 }, {duration: 5700})
+    var newPosition2 = this.anotherRandoPosition()
+    $('.rock').animate({ bottom: newPosition, left: 300 }, {duration: 1000})
+    $('.rock_1').animate({ bottom: newPosition, right: newPosition2 }, {duration: 1050})
+    $('.rock_2').animate({ top: newPosition, right: newPosition2 }, {duration: 1000})
+    $('.rock_3').animate({ bottom: newPosition, left: 650 }, {duration: 3000})
+    $('.rock_4').animate({ top: newPosition, right: newPosition2 }, {duration: 1950})
+    $('.rock_5').animate({ bottom: newPosition, right: newPosition2 }, {duration: 2000})
   }
 
   moveRock() {
     var rockItself = this
+    //this.movingRocks = setInterval(function(){rockItself.animateRock()}, 100)
     setInterval(function(){rockItself.animateRock()}, 100)
   }
+
+  // stopRock() {
+  //   clearInterval(this.movingRocks)
+  // }
 
 
 }
